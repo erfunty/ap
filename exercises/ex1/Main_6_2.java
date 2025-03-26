2e8572a0c140872654951c4ed31b9c59aabcd995
erfunty <er12fa34nty56.com>
2025-03-27 02:40:03 +0330

    e6.2d

--- exercises/ex1/Main_6_2.java
+++ exercises/ex1/Main_6_2.java
@@ -10,11 +10,20 @@ public class Main_6_2 {
         int odd=0;
         int even=0;
         int cumul=0;
+        int num=0;
+        int previous=-1;
         String cumulative=new String();
+        String pre=new String();
         System.out.print("enter a number:");
         while(in.hasNextInt()){
             System.out.print("enter a number:");
-            int num=in.nextInt();
+            if(previous!=num) {
+                previous = num;
+            }
+            else{
+                previous=-1;
+            }
+            num=in.nextInt();
             if(num<min){
                 min=num;
             }
@@ -30,10 +39,16 @@ public class Main_6_2 {
             }
             //////////////////////////////////
             cumul=cumul+num;
-            cumulative=cumulative+"    "+cumul;
+            cumulative=cumulative+"  "+cumul;
             //////////////////////////////////
+            if(previous==num){
+                pre=pre+"   "+previous;
+            }
+
         }
+
         System.out.println(cumulative);
+        System.out.println(pre);
         System.out.println("the bigest number:"+max+"\n"+"the smallest number:"+min);
         System.out.println("number of odd:"+odd+"\n"+"number of even:"+even);
     }
