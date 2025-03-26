8a836243bf148f01ba46655007a7e7b11df7c271
erfunty <er12fa34nty56.com>
2025-03-27 00:42:47 +0330

    e6.2b

--- exercises/ex1/Main_6_2.java
+++ exercises/ex1/Main_6_2.java
@@ -7,6 +7,8 @@ public class Main_6_2 {
         Scanner in=new Scanner(System.in);
         int min=2147483647;
         int max=0;
+        int odd=0;
+        int even=0;
         System.out.print("enter a number:");
         while(in.hasNextInt()){
             System.out.print("enter a number:");
@@ -17,8 +19,17 @@ public class Main_6_2 {
             if(num>max){
                 max=num;
             }
-
+            ///////////////////////////////////
+            if(num%2==0){
+                even++;
+            }
+            else{
+                odd++;
+            }
+            //////////////////////////////////
         }
+
         System.out.println("the bigest number:"+max+"\n"+"the smallest number:"+min);
+        System.out.println("number of odd:"+odd+"\n"+"number of even:"+even);
     }
 }
