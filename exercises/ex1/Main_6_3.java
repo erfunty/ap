15bd47c15743cf381134a53ca9bd53050875eb59
erfunty <er12fa34nty56.com>
2025-03-28 16:26:42 +0330

    e6.3e

--- exercises/ex1/Main_6_3.java
+++ exercises/ex1/Main_6_3.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 
 public class Main_6_3 {
     public static void main(String[] args) {
-       Scanner in = new Scanner(System.in);
-        System.out.println("enter string");
-        String str = in.nextLine();
-//       String str="Hello Fucking World";
+        Scanner in = new Scanner(System.in);
+       System.out.println("enter string");
+      String str = in.nextLine();
+     //String str="Hello Fucking World";
         for (int i = 0; i < str.length(); i++) {
             if (Character.isUpperCase(str.charAt(i))) {
                 System.out.print(str.charAt(i) + " ");
@@ -32,6 +32,13 @@ public class Main_6_3 {
             }
         }
         System.out.println(vowels);
+        /// //////////////
+        for (int i=0;i<str.length();i++) {
+            if ("AEIOUaeiou".indexOf(str.charAt(i)) != -1) {
+                System.out.print(i + " ");
+            }
+        }
+
 
 
     }
