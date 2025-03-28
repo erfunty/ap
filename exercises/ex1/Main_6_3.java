c693628a078efe6cccf0f60789270c9235a1106b
erfunty <er12fa34nty56.com>
2025-03-28 04:53:01 +0330

    e6.3c

--- exercises/ex1/Main_6_3.java
+++ exercises/ex1/Main_6_3.java
@@ -1,26 +1,32 @@
 package exercises.ex1;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Main_6_3 {
     public static void main(String[] args) {
-        Scanner in= new Scanner(System.in);
+       Scanner in = new Scanner(System.in);
         System.out.println("enter string");
-        String str=in.nextLine();
-//        String str="Hello Fucking World";
+        String str = in.nextLine();
+//       String str="Hello Fucking World";
         for (int i = 0; i < str.length(); i++) {
-            if(Character.isUpperCase(str.charAt(i))){
-                System.out.print(str.charAt(i)+" ");
+            if (Character.isUpperCase(str.charAt(i))) {
+                System.out.print(str.charAt(i) + " ");
             }
         }
         System.out.println("\n");
         ////////////////////////////////
-        for (int i = 1; i < str.length(); i+=2) {
-                System.out.print(str.charAt(i)+" ");
-            }
+        for (int i = 1; i < str.length(); i += 2) {
+            System.out.print(str.charAt(i) + " ");
         }
+        /// ////////////////////////
+        System.out.println("\n");
+        System.out.println(str.replaceAll("[AEIOUaeiou]", "_"));
+    }
 
 
 
 
 }
+
